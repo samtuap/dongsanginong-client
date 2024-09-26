67fe92b57c55fadc0199b47d2c1f921e80ba48b8
tteia <tteia.dl@gmail.com>
2024-09-26 16:19:56 +0900

    vuetify 3 설치

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).use(router).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
