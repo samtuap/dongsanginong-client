de399c238cb0faef360e54baf624bfddcd91de06
Sejeong Kim <clearrworld@gmail.com>
2024-09-27 17:55:50 +0900

    setting: mdi install

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,37 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import colors from 'vuetify/util/colors'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
+// mdi 이모지
+import { mdi } from 'vuetify/iconsets/mdi'; 
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+      themes: {
+          light: {
+              dark: false,
+              colors: {
+                  light_green: "#DBE098",
+                  deep_green: "#BCC07B",
+                  yellow: "#FFE2A6",
+                  light_orange: "#FFCC80",
+                  deep_orange: "#FFAF6E",
+              }
+          },
+      },
+  },  
+  icons: {
+  defaultSet: 'mdi',
+  sets: {
+      mdi,
+  }
+  },
+  
+});
