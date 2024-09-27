854da58177679bf1971fd8cfebae8a9289fbd26f
Sejeong Kim <clearrworld@gmail.com>
2024-09-27 18:12:56 +0900

    setting: vuetify color 세팅 후 테스트용 버튼 추가

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,6 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-import colors from 'vuetify/util/colors'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
