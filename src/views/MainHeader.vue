5d633548ed7f661b553c971419d7cd1600731a20
H-Yeji <yeji980603@naver.com>
2024-09-28 19:28:49 +0900

    feat: header 생성

---
+++ src/views/MainHeader.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-app-bar app dark dense>
+        <v-container class="bar" fluid>
+            <v-row>
+                <!-- 콜 전체를 왼쪽정 렬되게 해줌 -->
+                <v-col class="d-flex justify-start"> 
+                    <v-btn style="text-transform: none;">Farm</v-btn>
+                    <v-btn style="text-transform: none;">Live</v-btn>
+                    <v-btn style="text-transform: none;" v-if="!isSeller">Mypage</v-btn>
+                    <v-btn style="text-transform: none;" v-if="isSeller">MyFarm</v-btn>
+                </v-col>
+                <v-col class="text-center">
+                    <v-btn :to="{path:'/'}">
+                        <img src="/inongLogo.png" width=160 alt="Logo" class="main-logo-image" /> 
+                    </v-btn>
+                </v-col>
+                <!-- 콜 전체를 오른쪽 정렬되게 해줌 -->
+                <v-col class="d-flex justify-end">
+                <v-btn style="text-transform: none;" v-if="!isLogin" class="reduce-spacing">Login</v-btn>
+                <v-btn style="text-transform: none;" v-if="isLogin" class="reduce-spacing">Logout</v-btn>
+                <v-btn class="reduce-spacing">
+                    <img src="/notifications.png" width=30 alt="Logo" /> 
+                </v-btn>
+                <v-btn>
+                    <img src="/searchLogo.png" width=20 alt="Logo" /> 
+                </v-btn>
+                </v-col>
+            </v-row>
+
+        </v-container>
+    </v-app-bar>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            isLogin: false, // 로그인 여부 확인 
+            isSeller: false, // seller인지 member인지 여부 확인
+        }
+    },
+    created() {
+        // localStorage.setItem('token', 'temporary-token'); // 테스트용
+
+        const token = localStorage.getItem('token');
+        if (token) {
+        this.isLogin = true; 
+        } else {
+        this.isLogin = false; 
+        }
+    },
+};
+</script>
+
+<style scoped>
+.bar {
+    /* font-family: "Fredoka", sans-serif;  */
+    font-weight: 400;
+}
+.main-logo-image {
+    transform: translateY(-17px); 
+}
+.reduce-spacing {
+  padding-right: 0px;
+}
+</style>
