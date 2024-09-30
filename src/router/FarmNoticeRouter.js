00e2038bf0dd31f5c36b7523b0dc5c7e2bb9aafc
cogns <99cogns@naver.com>
2024-09-30 10:57:00 +0900

    chore: 수정

--- src/router/FarmNoticeRouter.js
+++
@@ -1,9 +0,0 @@
-import FarmNoticeList from "@/views/product/farmNotice/FarmNoticeList.vue";
-
-export const farmNoticeRouter = [
-    {
-        path: '/farm/:id/notice/list',
-        name: 'FarmNoticeList',
-        component: FarmNoticeList,
-    },
-]
