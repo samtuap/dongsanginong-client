838e0f8a5911da60d19e7a6fba46209a4ac3395c
cogns <99cogns@naver.com>
2024-09-30 10:08:03 +0900

    chore

--- vue.config.js
+++ vue.config.js
@@ -6,23 +6,5 @@ module.exports = defineConfig({
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
-  },
-  devServer: {
-    proxy: {
-      '/product-service': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        pathRewrite: { '^/product-service': '/product-service' },
-      },
-    },
-  },
-  // devServer: {
-  //   proxy: {
-  //     '/order-service': {
-  //       target: 'http://localhost:8080',
-  //       changeOrigin: true,
-  //       pathRewrite: { '^/order-service': '/order-service' },
-  //     },
-  //   },
-  // },
+  }
 })
