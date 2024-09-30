2aad8a53e832785227b9aa5d864652beadaee80c
vydndrl <vydndrl1234@naver.com>
2024-09-30 16:43:34 +0900

    feat: 판매자 회원가입 페이지

--- src/router/sellerRouter.js
+++ src/router/sellerRouter.js
@@ -1,9 +1,15 @@
 import SellerSalesDetail from "@/views/product/seller/SellerSalesDetail.vue"
+import SellerSignUp from "@/views/product/seller/SellerSignUp.vue"
 export const sellerRouter = [
 
     {
         path: '/seller/sales-detail',
         name: 'sellerSalesDetail',
         component: SellerSalesDetail
+    },
+    {
+        path: '/seller/sign-up',
+        name: 'sellerSignUp',
+        component: SellerSignUp
     },
 ]
