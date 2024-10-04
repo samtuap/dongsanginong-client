4013a63299600ba4f8d8987fc967dd8d396d9a14
cogns <99cogns@naver.com>
2024-10-04 12:02:26 +0900

    feat: 구매자 쿠폰 리스트 구현

--- src/router/memberRouter.js
+++ src/router/memberRouter.js
@@ -5,6 +5,7 @@ import MemberSignUp from "@/views/member/login/MemberSignUp.vue";
 import MemberMyPage from "@/views/member/myPage/MemberMyPage.vue";
 import MemberPhoneUpdate from "@/views/member/myPage/MemberPhoneUpdate.vue";
 import MemberAddressUpdate from "@/views/member/myPage/MemberAddressUpdate.vue";
+import MemberCouponList from "@/views/order/coupon/MemberCouponList.vue"
 
 export const memberRouter = [
   {
@@ -41,5 +42,10 @@ export const memberRouter = [
     path: "/member/update/address",
     name: "MemberAddressUpdate",
     component: MemberAddressUpdate,
+  },
+  {
+    path: '/coupons//downloaded-coupons',
+    name: 'MemberCouponList',
+    component: MemberCouponList,
   },
 ];
