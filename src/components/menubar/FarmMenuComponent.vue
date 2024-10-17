67f67bb2361f4da60cbadf2b5a93fb86d9e94879
tteia <tteia.dl@gmail.com>
2024-10-17 16:45:46 +0900

    fix: 농장 상세 정보 > 스크랩 수 제거

--- src/components/menubar/FarmMenuComponent.vue
+++ src/components/menubar/FarmMenuComponent.vue
@@ -13,7 +13,6 @@
               </div>
               <div class="farm-text" style="margin-top: -90px; margin-left: 110px; text-align: start;">
                 <span class="farm-name" style="margin-left: 10px;">{{ this.farmName }}</span><br>
-                <span style="font-size: 14px; color: grey; margin-left: 10px;"> 스크랩 수 {{ this.favoriteCount }}</span><br>
                 <span style="font-size: 14px; color: grey; margin-left: 10px;"> {{ this.farmIntro }}</span>
               </div>
             </v-row>
