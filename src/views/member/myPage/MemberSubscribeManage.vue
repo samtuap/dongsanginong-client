c2f52e45dfe9808981c8f1affff0beac2aefe582
Sejeong Kim <clearrworld@gmail.com>
2024-10-17 18:20:28 +0900

    feat: 정기결제 수단 등록 기능 연동

--- src/views/member/myPage/MemberSubscribeManage.vue
+++ src/views/member/myPage/MemberSubscribeManage.vue
@@ -2,10 +2,19 @@
     <div class="member-page" style="padding-right: 300px;">
         <MemberSidebar />
         <h3 style="margin-right: 74%;">내 구독 정보 관리</h3><br>
-        
+
         <!-- 자동결제 카드 정보 -->
         <div class="card-section">
             <div style="font-weight: 700; font-size:17px;">자동 결제 카드 정보</div>
+
+            <div v-if="billingKey === ''" style="margin-top: 15px;">
+                <p style="margin-bottom: 10px;">결제 수단이 없습니다. 등록해주세요.</p>
+                <v-btn @click="addPaymentMethod" style="border-radius: 50px;">결제 수단 등록하기</v-btn>
+            </div>
+            <div v-else style="align-items: center; display: flex; margin-top: 15px;">
+                <img :src="paymentMethodImageUrl" style="width: 60px;" />
+                <p>{{ paymentMethodType }}</p>
+            </div>
         </div>
         <br><br>
         <!-- 구독한 패키지 상품 -->
@@ -15,15 +24,20 @@
                 <v-card-text style="color: gray; margin-top: -25px;">회원님이 구독한 패키지입니다.</v-card-text>
                 <br>
                 <div style="display: flex; justify-content: center; align-items:center;">
-                    <v-btn class="slide-btn" icon="mdi-chevron-left" variant="plain" @click="prevProduct" :disabled="productOnboarding === 1"></v-btn>
+                    <v-btn class="slide-btn" icon="mdi-chevron-left" variant="plain" @click="prevProduct"
+                        :disabled="productOnboarding === 1"></v-btn>
                     <v-window v-model="productOnboarding" style="width: 1080px;">
                         <v-window-item v-for="n in productWindowCount" :key="`product-window-${n}`" :value="n">
                             <v-row class="d-flex justify-center">
-                                <v-col v-for="(packageProduct, index) in paginatedPackageProducts(n)" :key="index" cols="12" md="3" class="d-flex justify-center">
+                                <v-col v-for="(packageProduct, index) in paginatedPackageProducts(n)" :key="index"
+                                    cols="12" md="3" class="d-flex justify-center">
                                     <v-card variant="text" style="width:210px; height:300px;">
-                                        <v-img class="packageProduct-image" width="210px" height="250px" :src="packageProduct.imageUrl" alt="packageProduct 썸네일" cover @click="this.$router.push(`/product/${packageProduct.packageId}`)" />
+                                        <v-img class="packageProduct-image" width="210px" height="250px"
+                                            :src="packageProduct.imageUrl" alt="packageProduct 썸네일" cover
+                                            @click="this.$router.push(`/product/${packageProduct.packageId}`)" />
                                         <v-card-text style="padding: 0; margin-top: 10px;">
-                                            <span v-if="packageProduct.packageName.length > 10"> {{ packageProduct.packageName.substring(0, 10) }}...</span>
+                                            <span v-if="packageProduct.packageName.length > 10"> {{
+                                                packageProduct.packageName.substring(0, 10) }}...</span>
                                             <span v-else> {{ packageProduct.packageName }}</span>
                                             <div class="detail-container">
                                                 <a class="toDetail" @click="createReview(packageProduct.id)">후기 작성</a>
@@ -35,7 +49,8 @@
                             </v-row>
                         </v-window-item>
                     </v-window>
-                    <v-btn class="slide-btn" icon="mdi-chevron-right" variant="plain" @click="nextProduct" :disabled="!canGoToNextProduct"></v-btn>
+                    <v-btn class="slide-btn" icon="mdi-chevron-right" variant="plain" @click="nextProduct"
+                        :disabled="!canGoToNextProduct"></v-btn>
                 </div>
             </v-card>
         </div>
@@ -48,6 +63,7 @@
 import axios from 'axios';
 import MemberSidebar from '@/components/sidebar/MemberSidebar.vue';
 import ReviewCreate from '@/views/product/review/ReviewCreate.vue';
+import * as PortOne from "@portone/browser-sdk/v2";
 
 export default {
     components: {
@@ -62,9 +78,11 @@ export default {
             farmWindowCount: 3,
             productWindowCount: 3,
             isLoading: false,
-
             reviewModal: false,
             selectedPackageProductId: null,
+            billingKey: "",
+            paymentMethodType: "",
+            paymentMethodImageUrl: "",
         }
     },
     computed: {
@@ -76,6 +94,13 @@ export default {
     },
     async created() {
         await this.fetchSubscriptionPackageProducts();
+
+        const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/member-service/subscription/payment/method`);
+        this.billingKey = response.data.billingKey;
+        this.paymentMethodType = response.data.paymentMethodValue;
+        this.paymentMethodImageUrl = response.data.logoImageUrl;
+
+        console.log(response);
     },
     methods: {
         async fetchSubscriptionPackageProducts() {
@@ -104,7 +129,52 @@ export default {
         },
         withdrawSubscribe() {
             // 영수증 페이지로 보내주기
-        }
+        },
+        async addPaymentMethod() {
+
+            // issueId 생성
+            const memberId = localStorage.getItem("memberId");
+            const issueId = memberId + "_" + crypto.randomUUID();
+
+            try {
+                const res = await PortOne.requestIssueBillingKey({
+                    storeId: `${process.env.VUE_APP_PORTONE_STORE_ID}`, // 고객사 storeId로 변경해주세요.
+                    channelKey: `${process.env.VUE_APP_PORTONE_CHANNEL_KEY}`, // 콘솔 결제 연동 화면에서 채널 연동 시 생성된 채널 키를 입력해주세요.
+                    billingKeyMethod: "EASY_PAY",
+                    issueId: issueId,
+                    issueName: `동상이농 정기 결제 수단 등록`,
+                    orderName: `동상이농 정기 결제 수단 등록`,
+                    paymentId: "test_payment_001",
+                    totalAmount: 0,
+                    curreny: "KRW",
+                    paymentMethod: "EASY_PAY",
+                    redirectUrl: `${process.env.VUE_APP_MY_URL}/payment-done`,
+                    offerPeriod: {
+                        interval: "1m"
+                    },
+                    customer: {
+                        fullName: "김세정",
+                        phoneNumber: "010-3104-4841",
+                        email: "clearrworld@gmail.com",
+                    },
+                });
+
+
+                this.billingKey = res.billingKey;
+                this.successModal = true;
+                this.paymentMethod = 'KAKAOPAY'; // TODO: 추후 확장 가능성
+
+                const body = {
+                    'billingKey': this.billingKey,
+                    'paymentMethodType': this.paymentMethod
+                }
+
+                await axios.post(`${process.env.VUE_APP_API_BASE_URL}/member-service/subscription/payment/method`, body);
+            } catch (e) {
+                this.failModal = true;
+                console.log(e);
+            }
+        },
     }
 }
 </script>
@@ -118,6 +188,7 @@ export default {
     padding: 40px;
     flex-direction: column;
 }
+
 .subscribe-section {
     display: flex;
     flex-direction: column;
@@ -128,6 +199,7 @@ export default {
     margin-left: 20%;
     background-color: white;
 }
+
 .card-section {
     display: flex;
     flex-direction: column;
@@ -138,6 +210,7 @@ export default {
     margin-left: 20%;
     background-color: white;
 }
+
 .toDetail {
     color: #ca7f5a;
     font-size: 12px;
@@ -145,7 +218,7 @@ export default {
     text-decoration: none;
 }
 
-.detail-container{
+.detail-container {
     display: flex;
     justify-content: flex-start;
     gap: 10px;
@@ -154,6 +227,7 @@ export default {
 .slide-btn {
     margin-bottom: 40px;
 }
+
 .packageProduct-image {
     cursor: pointer;
 }
