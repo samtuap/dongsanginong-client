3d6f1a62d0beeb17169843ee803173c619997b35
H-Yeji <yeji980603@naver.com>
2024-10-18 12:21:18 +0900

    chore: member 사이드바 수정

--- src/components/sidebar/MemberSidebar.vue
+++ src/components/sidebar/MemberSidebar.vue
@@ -9,8 +9,8 @@
                     <v-btn class="cat_btn" :to="{path:'/member/my-page'}">마이페이지</v-btn><br>
                     <v-btn class="cat_btn" :to="{path:'/member/update/info'}">내 정보 수정</v-btn><br>
                     <v-btn class="cat_btn" :to="{path: '/member/subscribe-management'}">정기구독 관리</v-btn><br>
-                    <v-btn class="cat_btn" :to="{path:'/coupons/downloaded-coupons'}">보유한 쿠폰 조회</v-btn><br>
                     <v-btn class="cat_btn" :to="{path:'/member/favorite'}">스크랩 농장 / 위시리스트</v-btn><br>
+                    <v-btn class="cat_btn" :to="{path:'/coupons/downloaded-coupons'}">보유한 쿠폰 조회</v-btn><br>
                     <v-btn class="cat_btn" :to="{path:'/member/payment/list'}">결제 내역 조회</v-btn><br>
                     <v-btn class="cat_btn" :to="{path:'/member/delivery/list'}">배송 조회</v-btn><br>
                 </v-col>
@@ -36,7 +36,7 @@ export default {
 .cat_btn{
     border: none;
     box-shadow: none;
-    justify-content: center;
+    justify-content: start;
     width: 100%;
 }
 </style>
